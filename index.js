514733f29e511eee4f60f24715ab564f3e8bc846
saifkhan <ksaif6150@outlook.com>
2023-08-01 10:08:35 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+import inquirer from "inquirer";
+import qr from "qr-image";
+import fs from "fs";
+
+inquirer
+  .prompt([
+    {
+        message : "Enter the URL?",
+        name : "URL", 
+    }
+  ])
+  .then((answers) => {
+    const url = answers.URL;
+    var qr_png = qr.image(url);
+    qr_png.pipe(fs.createWriteStream('i_love_qr.png'));
+
+
+    fs.writeFile("URL.txt",url,(err)=>{
+        if(err) throw err;
+        console.log("THis file has been saved.");
+    });
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
